91c82edf0e0800a46f093ab1693f1a0dd9cee977
Iheruome Michael <32968231+Javaworld22@users.noreply.github.com>
2025-09-21 05:51:52 +0100

    Update pau.py

--- pau.py
+++ pau.py
@@ -107,6 +107,9 @@ with col4:
 
 if st.session_state.mbar == 0 and st.session_state.mnth == 0:
     mdata = df_2024
+    st.info("Extremely High Usage: Recorded daily consumption was exceptionally high, ranging from an estimated 700 to 900 kWh on full days. For context, this is many times higher than a typical household.")
+    st.info("Significant Spike: The most notable event was a massive 77 kWh spike at 7 PM on June 25th, indicating a singular, high-power event like powering heating elements or higher inductive loads.")
+    st.info("")
 elif st.session_state.mbar == 0 and st.session_state.mnth == 1:
     mdata = df_2024_sept
 elif st.session_state.mbar == 1 and st.session_state.mnth == 0:
@@ -240,4 +243,5 @@ st.selectbox("Select Day", mday)
 
 mMonth = ("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sept","Oct","Nov","Dec")
 st.selectbox("Select Month", mMonth)
+
 
